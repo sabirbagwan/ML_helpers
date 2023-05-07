811624ee14999ce0b5d7ac616c736e1965685d22
Sabir Bagwan <106504464+sabirbagwan@users.noreply.github.com>
2023-05-07 08:43:39 +0530

    added hyperparameter tuning

--- Model/Model_Regressio(import&model).py
+++ Model/Model_Regressio(import&model).py
@@ -48,6 +48,43 @@ for name, model in models.items():
     print(name + " trained.")
 
 
+
+
+################# Hyperparamater tuning 
+from sklearn.model_selection import GridSearchCV
+
+params = {
+    "Linear Regression": {},
+    "Linear Regression (L2 Regularization)": {"alpha": [0.01, 0.1, 1, 10]},
+    "Linear Regression (L1 Regularization)": {"alpha": [0.01, 0.1, 1, 10]},
+    "K-Nearest Neighbors": {"n_neighbors": [3, 5, 7, 9]},
+    "Neural Network": {"hidden_layer_sizes": [(50,), (100,), (50, 50), (100, 50)], "alpha": [0.01, 0.001]},
+    "Support Vector Machine (Linear Kernel)": {"C": [0.1, 1, 10]},
+    "Support Vector Machine (RBF Kernel)": {"C": [0.1, 1, 10], "gamma": [0.01, 0.1, 1]},
+    "Decision Tree": {"max_depth": [3, 5, 7, 9]},
+    "Random Forest": {"n_estimators": [50, 100, 150], "max_depth": [3, 5, 7, 9]},
+    "Gradient Boosting": {"learning_rate": [0.01, 0.1, 1], "max_depth": [3, 5, 7, 9]},
+    "XGBoost": {"learning_rate": [0.01, 0.1, 1], "max_depth": [3, 5, 7, 9]},
+    "LightGBM": {"learning_rate": [0.01, 0.1, 1], "max_depth": [3, 5, 7, 9]},
+    "CatBoost": {"learning_rate": [0.01, 0.1, 1], "max_depth": [3, 5, 7, 9]}
+}
+
+for name, model in models.items():
+    if name in params:
+        print("Tuning hyperparameters for " + name)
+        param_grid = params[name]
+        grid_search = GridSearchCV(model, param_grid=param_grid, cv=5, n_jobs=-1)
+        grid_search.fit(X_train, y_train)
+        print("Best parameters:", grid_search.best_params_)
+        models[name] = grid_search.best_estimator_
+    else:
+        print("Skipping " + name + " as it does not have any hyperparameters to tune.")
+
+
+
+
+
+
 ################### RMSE & R^2 ###################
 
 for name, model in models.items():
